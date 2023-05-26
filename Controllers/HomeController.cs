fbc877746f8d0102f976b9c9700ce97da23244cc
tresor <tshims79@gmail.com>
2023-05-26 18:26:14 +0200

    We can register and see the thank you page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,8 +11,16 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpGet]
     public ViewResult RsvpForm()
     {
         return View();
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+        Repository.AddResponse(guestResponse);
+        return View("Thanks",guestResponse);
     }
 }
